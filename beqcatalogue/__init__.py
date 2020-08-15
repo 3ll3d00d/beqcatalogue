8158c28429ae48a67566634e0ba7dc55094a1774
3ll3d00d <mattkhan@gmail.com>
2020-08-15 19:08:37 +0100

    detect multiple formats in a single post

--- beqcatalogue/__init__.py
+++ beqcatalogue/__init__.py
@@ -22,6 +22,7 @@ def write_text(p_id, txt):
 
 
 def extract_from_html():
+    ''' extracts the content links from the 1st few posts of the avs thread '''
     ignored = ['Take the Red Pill (BassEQ)', 'BassEQ Demo Clips', 'TR Curves']
     by_id = OrderedDict()
     first_page = get_text('first')
@@ -52,7 +53,13 @@ def scrub_links(txt):
     return txt
 
 
-def extract_from_md_line(by_id, idx, trimmed):
+def extract_from_md_line(idx, trimmed):
+    '''
+    extracts the relevant info from the line of markdown
+    :param idx:
+    :param trimmed:
+    :return:
+    '''
     match = re.search(r"^\[([\w\s\d.:\-'\"’&,/!·•+()]+)]\((?:https://[\w\d./\-\.?=#]+post-)(\d+)( \".*\")?\)", trimmed)
     if match:
         name, post_id, extras = match.groups()
@@ -74,9 +81,10 @@ def extract_from_md_line(by_id, idx, trimmed):
         #     print(f"Missing release_date at line {idx + 1} [{name}] | {extra_fragment}")
         if not tags:
             print(f"Missing tags at line {idx + 1} [{name}] | {extra_fragment}")
-        by_id[post_id] = (name, release_date, tags)
+        return post_id, name, release_date, tags
     else:
         print(f"Ignoring line {idx + 1} : {trimmed}")
+        return None
 
 
 def cleanse_release_date(idx, match, trimmed):
@@ -128,7 +136,9 @@ def extract_from_md():
             trimmed = l.strip()
             if trimmed and trimmed[0] == '[':
                 for f in fix_formatting(idx, trimmed):
-                    extract_from_md_line(by_id, idx, f)
+                    post_id, *vals = extract_from_md_line(idx, f)
+                    if post_id:
+                        by_id[post_id] = vals
     return by_id
 
 
@@ -188,20 +198,20 @@ def extract_titles():
         return extract_from_html()
 
 
-def format_post_text(pt):
-    txt = [p.text().strip() for p in pt if p.text().strip()]
-    for t in txt:
-        m = re.search(r".*B(?:ass|ASS)?EQ *([\w\s.:\-'\"’&,/!·()+]+) +\((\d{4})\)?(?: |\n)+(.*).*", t)
-        if m:
-            name = m.group(1)
-            year = m.group(2)
-            format = m.group(3).replace('\u200b', '')
-            return name, year, format
-    txt = [t.replace('BEQ', '').replace('BassEQ', '').replace('BASSEQ', '').replace('&#8203;', '') for t in txt]
-    return '\n'.join(txt)
+def format_post_text(txt):
+    ''' formats the basseq post header, assumes a specific format '''
+    m = re.search(r".*B(?:ass|ASS)?EQ *([\w\s.:\-'\"’&,/!·()+]+) +\((\d{4})\)?(?: |\n)+(.*).*", txt)
+    if m:
+        name = m.group(1)
+        year = m.group(2)
+        format = m.group(3).replace('\u200b', '')
+        return name, year, format
+    else:
+        return txt
 
 
 def get_post(post_id, url):
+    ''' gets the post html from either the cache or from AVS directly. '''
     html = get_text(post_id)
     should_cache = False
     if html is None:
@@ -211,114 +221,197 @@ def get_post(post_id, url):
             should_cache = True
         else:
             print(f"Failed to get {url} - {r.status_code}")
-    return html, should_cache
+    if should_cache:
+        write_text(post_id, html)
+    return html
 
 
-posts = extract_titles()
-# posts = extract_from_html()
-# p2 = extract_from_md()
-#
-# for k,v in p2.items():
-#     if k not in posts:
-#         print(f"Missing from html {k} -> {v}")
-# for k,v in posts.items():
-#     if k not in p2:
-#         print(f"Missing from md {k} -> {v}")
-#
-# exit(1)
-print(f"Extracted {len(posts.keys())} catalogue entries")
+def find_post_content(post_id, tree):
+    '''
+    extracts the img links out of a post.
+    :param post_id: the post id.
+    :param tree: the content tree.
+    :return: all links, links inside a spoiler
+    '''
+    links_by_txt = OrderedDict()
+    chunks = []
+    post_matcher = f"article[data-content={post_id}] article[qid=\"post-text\"]"
+    post_html = tree.css_first(post_matcher, strict=True).html
+    post_node = HTMLParser(post_html)
+    # have to parse this manually because node order is not preserved by this library
+    # img_or_txt = f"b,img[data-src],div[class=\"bbMediaWrapper\"] a"
+    # nodes = post_node.css(img_or_txt)
+    # for node in nodes:
+    for node in post_node.root.traverse(include_text=False):
+        if node.tag == 'b':
+            if not chunks or next((x for x in chunks[-1] if x[0:4] == 'IMG|'), None) is not None:
+                chunks.append([node.text().strip()])
+            else:
+                chunks[-1].append(node.text().strip())
+        elif node.tag == 'img' and 'data-src' in node.attributes:
+            chunks[-1].append(f"IMG|{node.attributes['data-src']}")
+        elif node.tag == 'a' and 'href' in node.attributes:
+            href = node.attributes['href']
+            if '%5B' in href and is_descendant_of(node.parent, lambda n: n and n.tag == 'div' and 'class' in n.attributes and n.attributes['class'] == 'bbMediaWrapper'):
+                chunks[-1].append(f"IMG|{href[0:href.index('%5B')]}")
+
+    for idx, chunk in enumerate(chunks):
+        if len(chunk) < 2:
+            print(f"Ignoring chunk {idx} in {post_id}")
+        else:
+            links_by_txt[chunk[0]] = chunk[1:]
+
+    spoiler_links = [img.attributes['data-src'] for img in tree.css(f"article[data-content={post_id}] div[class=\"bbCodeSpoiler\"] img[data-src]")]
+    return links_by_txt, spoiler_links
 
 
-with open('../tmp/delta.txt', mode='w+') as delta:
-    with open('../tmp/errors.txt', mode='w+') as err:
-        with open('../docs/index.md', mode='w+') as cat:
-            with open('../docs/database.csv', 'w+', newline='') as db:
-                db_writer = csv.writer(db)
-                db_writer.writerow(['Title', 'Release Date', 'Production Year', 'Format', 'AVS', 'Catalogue', 'blu-ray.com'])
-                print(f"| Title | Release Date | Production Year | Format | Discussion | Lookup | Notes |", file=cat)
-                print(f"|-|-|-|-|-|-|-|", file=cat)
+def is_descendant_of(node, predicate):
+    '''
+    :param node: the node to test
+    :param predicate: the predicate to test
+    :return: true if the node meets the predicate or its parent does.
+    '''
+    return predicate(node) or is_descendant_of(node.parent, predicate)
 
-                for k, v in posts.items():
-                    post_id = f"post-{k}"
-                    url = f"https://www.avsforum.com/threads/bass-eq-for-filtered-movies.2995212/{post_id}"
 
-                    html, should_cache = get_post(post_id, url)
-                    if isinstance(v, tuple):
-                        content_name = v[0]
-                        release_date = v[1]
-                        content_format = v[2]
-                    else:
-                        content_name = v
-                        release_date = ''
-                        content_format = ''
-                    production_year = ''
-                    release_year = ''
-                    bd_url = ''
-                    if release_date != '':
-                        release_year = datetime.strptime(release_date, '%Y-%m-%d').strftime('%Y')
+def process_content():
+    post_id = f"post-{k}"
+    url = f"https://www.avsforum.com/threads/bass-eq-for-filtered-movies.2995212/{post_id}"
+    html = get_post(post_id, url)
+    if isinstance(v, list):
+        content_name = v[0]
+        release_date = v[1]
+        content_format = v[2]
+    else:
+        content_name = v
+        release_date = ''
+        content_format = ''
+    production_year = ''
+    release_year = ''
+    bd_url = ''
+    if release_date != '':
+        release_year = datetime.strptime(release_date, '%Y-%m-%d').strftime('%Y')
+    if html is not None:
+        tree = HTMLParser(html)
+        found = False
+        if tree.body is not None:
+            links_by_txt, spoiler_links = find_post_content(post_id, tree)
+            if links_by_txt:
+                found = True
+                with open(f"../docs/{k}.md", mode='w+') as sub:
+                    content_format, production_year = generate_content_page(content_format, content_name, links_by_txt,
+                                                                            production_year, release_date,
+                                                                            release_year, spoiler_links, sub, url)
+                bd_url = generate_index_entry(content_format, content_name, production_year, release_date,
+                                              release_year, url, len(links_by_txt.keys()) > 1)
+        if not found:
+            print(f"Failed to find content in {url} for {content_name}")
+            print(f"| [{content_name}](./{k}.md) | | | | | [AVS Post]({url}) | | **NO DATA** |", file=cat)
+            with open(f"../docs/{k}.md", mode='w+') as sub:
+                print(f"**NO CONTENT FOUND**", file=sub)
+    db_writer.writerow([content_name, release_date, production_year, content_format, url,
+                        f"https://beqcatalogue.readthedocs.io/en/latest/{k}/", bd_url])
 
-                    if html is not None:
-                        tree = HTMLParser(html)
-                        found = False
-                        if tree.body is not None:
-                            links = [img.attributes['data-src'] for img in tree.css(f"article[data-content={post_id}] img[data-src]")]
-                            if not links:
-                                urls = [a.attributes['href'] for a in tree.css(f"article[data-content={post_id}] div[class=\"bbMediaWrapper\"] a")]
-                                if urls:
-                                    links = [l[0:l.index('%5B')] for l in urls]
-                            if links:
-                                found = True
-                                post_text = tree.css(f"article[data-content={post_id}] article[qid=\"post-text\"] div[class=\"bbWrapper\"]")
-                                with open(f"../docs/{k}.md", mode='w+') as sub:
-                                    print(f"# {content_name}", file=sub)
-                                    print("", file=sub)
-                                    print(f"[Discussion Post]({url})", file=sub)
-                                    print("", file=sub)
-                                    if release_date != '':
-                                        print(f"* Release Date: {release_date}", file=sub)
-                                    formatted = format_post_text(post_text)
-                                    if isinstance(formatted, tuple):
-                                        if content_format == '':
-                                            content_format = formatted[2]
-                                        if content_format != formatted[2]:
-                                            print(f"{content_name},{content_format},{formatted[2]}", file=delta)
-                                        if release_year != '' and release_year != formatted[1]:
-                                            print(f"{content_name},{release_year},{formatted[1]}", file=delta)
-                                        production_year = formatted[1]
-                                        print(f"* Production Year: {production_year}", file=sub)
-                                        print(f"* Format: {content_format}", file=sub)
-                                        print("", file=sub)
-                                    else:
-                                        print(f"* Production Year: {production_year}", file=sub)
-                                        print(f"* Format: {content_format}", file=sub)
-                                        print(formatted, file=sub)
-                                        print("", file=sub)
-                                    for idx, l in enumerate(links):
-                                        print(f"![img {idx}]({l})", file=sub)
-                                        print('', file=sub)
-                                if len(links) != 2:
-                                    print(f"{url} - {content_name} - {len(links)}", file=err)
-                                query = content_name
-                                escaped = parse.quote(query)
-                                # y:{year}
-                                mdb_url = f"https://www.themoviedb.org/search?query={escaped}"
-                                rt_url = f"https://www.rottentomatoes.com/search?search={escaped}"
-                                # &yearfrom={production_year}&yearto={production_year}
-                                # &releaseyear=2006
-                                release_filter = ''
-                                if release_year != '':
-                                    release_filter = f"&releaseyear={release_year}"
-                                bd_url = f"https://www.blu-ray.com/movies/search.php?keyword={escaped}{release_filter}&submit=Search&action=search&"
-                                print(f"| [{content_name}](./{k}.md) | {release_date} | {production_year} | {content_format} | [avsforum]({url}) | [blu-ray]({bd_url}) [themoviedb]({mdb_url}) [rottentoms]({rt_url}) | |",
-                                      file=cat)
-                        if not found:
-                            print(f"Failed to find content in {url} for {content_name}")
-                            print(f"| [{content_name}](./{k}.md) | | | | [AVS Post]({url}) | | **NO DATA** |", file=cat)
-                            with open(f"../docs/{k}.md", mode='w+') as sub:
-                                print(f"**NO CONTENT FOUND**", file=sub)
-                        if should_cache is True:
-                            write_text(post_id, html)
 
-                    db_writer.writerow([content_name, release_date, production_year, content_format, url, f"https://beqcatalogue.readthedocs.io/en/latest/{k}/", bd_url])
+def generate_index_entry(content_format, content_name, production_year, release_date, release_year, url, multiformat):
+    ''' dumps the summary info to the index page '''
+    escaped = parse.quote(content_name)
+    # y:{year}
+    mdb_url = f"https://www.themoviedb.org/search?query={escaped}"
+    rt_url = f"https://www.rottentomatoes.com/search?search={escaped}"
+    # &yearfrom={production_year}&yearto={production_year}
+    # &releaseyear=2006
+    release_filter = ''
+    if release_year != '':
+        release_filter = f"&releaseyear={release_year}"
+    bd_url = f"https://www.blu-ray.com/movies/search.php?keyword={escaped}{release_filter}&submit=Search&action=search&"
+    print(
+        f"| [{content_name}](./{k}.md) | {release_date} | {production_year} | {content_format} | {'Yes' if multiformat else 'No'} | [avsforum]({url}) | [blu-ray]({bd_url}) [themoviedb]({mdb_url}) [rottentoms]({rt_url}) | |",
+        file=cat)
+    return bd_url
 
-                print('', file=cat)
+
+def generate_content_page(content_format, content_name, links_by_text, production_year, release_date, release_year,
+                          spoiler_links, sub, url):
+    ''' prints the md content page '''
+    print(f"# {content_name}", file=sub)
+    print("", file=sub)
+    print(f"[Discussion Post]({url})", file=sub)
+    print("", file=sub)
+    if release_date != '':
+        print(f"* Release Date: {release_date}", file=sub)
+    first = True
+    img_idx = 0
+    for pt, image_links in links_by_text.items():
+        if image_links:
+            if first:
+                formatted = format_post_text(pt)
+                if isinstance(formatted, tuple):
+                    if content_format == '':
+                        content_format = formatted[2]
+                    if content_format != formatted[2]:
+                        print(f"{content_name},{content_format},{formatted[2]}", file=delta)
+                    if release_year != '' and release_year != formatted[1]:
+                        print(f"{content_name},{release_year},{formatted[1]}", file=delta)
+                    production_year = formatted[1]
+                    print(f"* Production Year: {production_year}", file=sub)
+                    print('', file=sub)
+                    print(f"## {content_format}", file=sub)
+                    print('', file=sub)
+                else:
+                    print(f"* Production Year: {production_year}", file=sub)
+                    print('', file=sub)
+                    print(f"## {content_format}", file=sub)
+                    print(formatted, file=sub)
+                    print('', file=sub)
+            else:
+                pt = pt if pt[-1] != ':' else pt[0:-1]
+                print(f"## {pt}", file=sub)
+                print('', file=sub)
+
+            i = 0
+            for l in image_links:
+                if len(l) >= 4 and l[0:4] == 'IMG|':
+                    print(f"![img {i + img_idx}]({l[4:]})", file=sub)
+                    print('', file=sub)
+                    i += 1
+                else:
+                    print(f"Ignoring post content {url} - {l}")
+            img_idx += i
+            if i > 2:
+                print(f"{url} - {content_name} - {i}", file=excess)
+            if spoiler_links:
+                print(f"{url} - {content_name} - {len(spoiler_links)}", file=spoilers)
+        first = False
+    return content_format, production_year
+
+
+if __name__ == '__main__':
+    posts = extract_titles()
+    # posts = extract_from_html()
+    # p2 = extract_from_md()
+    #
+    # for k,v in p2.items():
+    #     if k not in posts:
+    #         print(f"Missing from html {k} -> {v}")
+    # for k,v in posts.items():
+    #     if k not in p2:
+    #         print(f"Missing from md {k} -> {v}")
+    #
+    # exit(1)
+    print(f"Extracted {len(posts.keys())} catalogue entries")
+
+    with open('../tmp/spoilers.txt', mode='w+') as spoilers:
+        with open('../tmp/delta.txt', mode='w+') as delta:
+            with open('../tmp/excess.txt', mode='w+') as excess:
+                with open('../docs/index.md', mode='w+') as cat:
+                    with open('../docs/database.csv', 'w+', newline='') as db:
+                        db_writer = csv.writer(db)
+                        db_writer.writerow(['Title', 'Release Date', 'Production Year', 'Format', 'AVS', 'Catalogue', 'blu-ray.com'])
+                        print(f"| Title | Release Date | Production Year | Format | Multiformat? | Discussion | Lookup | Notes |", file=cat)
+                        print(f"|-|-|-|-|-|-|-|-|", file=cat)
+
+                        for k, v in posts.items():
+                            process_content()
+
+                        print('', file=cat)
