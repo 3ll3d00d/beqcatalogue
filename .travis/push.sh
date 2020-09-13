2ec4d063ddea734311a337d449ddde92d3855db7
3ll3d00d <mattkhan@gmail.com>
2020-09-13 21:11:30 +0100

    updated docs

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git checkout -b feature/travis-test
+  git add -A
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/3ll3d00d/beqcatalogue.git > /dev/null 2>&1
+  git push -set-upstream origin-pages HEAD
+}
+
+setup_git
+commit_website_files
+upload_files
