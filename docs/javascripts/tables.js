204ad3467c0406c39946024f7205575f2a40b3ae
3ll3d00d <mattkhan@gmail.com>
2024-02-10 15:14:06 +0000

    added table filter

--- docs/javascripts/tables.js
+++ docs/javascripts/tables.js
@@ -1,6 +1,11 @@
-app.location$.subscribe(function() {
-    var tables = document.querySelectorAll("article table")
+document$.subscribe(function() {
+    var tables = document.querySelectorAll("article table:not([class])")
     tables.forEach(function(table) {
-        new Tablesort(table)
+        new Tablesort(table);
+        var tf = new TableFilter(table, {
+            base_path: '',
+            ignore_diacritics: true,
+        });
+        tf.init();
     })
 })
