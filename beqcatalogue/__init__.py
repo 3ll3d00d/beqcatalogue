1107493989329d433a317cb74ccd3de0c054c276
3ll3d00d <mattkhan@gmail.com>
2026-03-27 22:07:08 +0000

    fix case of TV path

--- beqcatalogue/__init__.py
+++ beqcatalogue/__init__.py
@@ -780,8 +780,7 @@ if __name__ == '__main__':
         ('kaelaria', '.input/kaelaria/Beq1/', 'movies', 'tv'),
         ('remixmark', '.input/remixmark/miniDSPBEQ/', 'Movie BEQs', 'TV BEQs'),
         ('mikejl', '.input/mikejl/xml/', 'Movies', 'TV'),
-        # enable when metadata is added
-        ('bombaycat007', '.input/bombaycat007/miniDSPBEQ/', 'Movie BEQs', 'TV BEQs')
+        ('bombaycat007', '.input/bombaycat007/miniDSPBEQ/', 'Movie BEQs', 'TV BEQS')
     ]
 
     all_authors = [a[0] for a in repo_configs]
